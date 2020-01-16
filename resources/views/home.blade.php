7a65482a2ee7d0a41365446e5a728d8cf766e88e
mikkp17 <mikkeleigner@gmail.com>
2020-01-16 16:51:02 +0100

    Downloading files implemented

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    You are logged in!
+
+                    <div class="links">
+                        <br>
+                        <a href="{{ url('/create') }}">Create a new excel sheet</a>
+                        <br>
+                        <a href="{{ url('/download') }}">Download an existing excel sheet</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/create', 'FileController@create');
+Route::get('/download', 'FileController@download');
+Route::get('/download/{id}', 'FileController@download');
